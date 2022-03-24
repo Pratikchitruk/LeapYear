6cb3f346bbe6f2adad3786b0777ad074d8453d4d
Pratikchitruk <99415272+Pratikchitruk@users.noreply.github.com>
2022-03-24 12:13:23 +0530

    LeapYear.cs push for LeapYear

---
+++ LeapYear.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYearDemo
+{
+    public class LeapYear
+    {
+        public static void FindLeapYear()
+        {
+            Console.WriteLine("enter year :");
+            
+            int year = int.Parse(Console.ReadLine());
+
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) Console.WriteLine("{0} is a leap year.", year);
+            else Console.WriteLine("{0} is not leap year");
+            Console.ReadLine();
+        }
+    }
+}
